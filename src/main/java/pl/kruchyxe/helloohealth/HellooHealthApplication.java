fa9d731a0c80b7c6906f161ac3040327d01a4936
Kruchyxe <krzysiekmilczanowski@interia.eu>
2022-06-24 20:31:32 +0200

    Initial commit

---
+++ src/main/java/pl/kruchyxe/helloohealth/HellooHealthApplication.java
@@ -0,0 +1,13 @@
+package pl.kruchyxe.helloohealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellooHealthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellooHealthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kruchyxe/helloohealth/HellooHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kruchyxe.helloohealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellooHealthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
