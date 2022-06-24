df55f97afcfb52511181d3e71bea904bbfdd0d86
Kruchyxe <krzysiekmilczanowski@interia.eu>
2022-06-24 21:51:25 +0200

    commit creating validation on user class

--- src/main/java/pl/kruchyxe/helloohealth/model/User.java
+++ src/main/java/pl/kruchyxe/helloohealth/model/User.java
@@ -3,6 +3,9 @@ package pl.kruchyxe.helloohealth.model;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
@@ -15,11 +18,20 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotBlank(message = "First name may not be blank")
+    @Size(min = 1, max = 40)
     private String firstName;
 
+    @NotBlank(message = "Last name may not be blank")
+    @Size(min = 1, max = 45)
     private String lastName;
 
+    @NotBlank(message = "Email may not be blank")
+    @Size(min=1,max=45)
+    @Email(message = "Please provide valid email address")
     private String email;
 
+    @NotBlank(message = "Password may not be blank")
+    @Size(min=10)
     private String password;
 }
