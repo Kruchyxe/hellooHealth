9108822c7cb433ae038c075b5a21744780b8522d
Kruchyxe <krzysiekmilczanowski@interia.eu>
2022-06-24 22:08:37 +0200

    commit creating many too many relation

--- src/main/java/pl/kruchyxe/helloohealth/model/User.java
+++ src/main/java/pl/kruchyxe/helloohealth/model/User.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.Collection;
 
 @Entity
 @Table(name = "users")
@@ -15,7 +16,7 @@ import javax.validation.constraints.Size;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank(message = "First name may not be blank")
@@ -34,4 +35,14 @@ public class User {
     @NotBlank(message = "Password may not be blank")
     @Size(min=10)
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name="users_roles",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id"))
+
+    private Collection<Role> roles;
 }
