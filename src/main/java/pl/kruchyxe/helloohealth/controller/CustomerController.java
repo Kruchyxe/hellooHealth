22d3b9fb93b43d676c0b9ee353b9033febf1069d
Kruchyxe <krzysiekmilczanowski@interia.eu>
2022-07-03 17:19:43 +0200

    commit customerControoller new methods fixed

--- src/main/java/pl/kruchyxe/helloohealth/controller/CustomerController.java
+++ src/main/java/pl/kruchyxe/helloohealth/controller/CustomerController.java
@@ -1,9 +1,9 @@
 package pl.kruchyxe.helloohealth.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import pl.kruchyxe.helloohealth.model.Customer;
 import pl.kruchyxe.helloohealth.service.CustomerService;
 
@@ -12,26 +12,40 @@ import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("customers")
 public class CustomerController {
 
     private CustomerService customerService;
 
 
-    @GetMapping("/customers")
+    @GetMapping("")
     public List<Customer> getCustomer() {
         return customerService.getAllCustomers();
 
     }
 
-    @GetMapping("/customers/{id}")
+    @GetMapping("/{id}")
     public Optional<Customer> getSingleCustomer(@PathVariable long id) {
         return Optional.of(customerService.findCustomerById(id)
-                .orElseThrow());
+                .orElseThrow(() -> {
+                    throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Entity not found");
+                }));
+    }
+
+    @GetMapping("/delete/{id}")
+    public void removeCustomer(@PathVariable Long id) {
+
+        try {
+            customerService.deleteCustomerById(id);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
 
     }
 
-
-
-
+    @PostMapping("/add")
+    public void addingCustomer(@RequestBody Customer customer){
+        customerService.addCustomer(customer);
+    }
 
 }
